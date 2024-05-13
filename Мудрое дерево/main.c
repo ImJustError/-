d94db09ee2d02ab38ae98813822763231b9d78cc
danii <danii@>
2024-05-13 18:47:05 +0300

    hui

--- Мудрое дерево/main.c
+++ Мудрое дерево/main.c
@@ -3,14 +3,21 @@
 
 
 int main() {
-	node maintree = init();
-	addel(&maintree, 6);
-	addel(&maintree, 2);
-	addel(&maintree, 7);
+	node maintree = init(NULL);
+	addel(&maintree, 8);
 	addel(&maintree, 3);
+	addel(&maintree, 10);
+	addel(&maintree, 1);
+	addel(&maintree, 6);
 	addel(&maintree, 4);
-	addel(&maintree, 5);
-	printf("%d", findel(&maintree, 10));
+	addel(&maintree, 7);
+	addel(&maintree, 13);
+	printDerevo(&maintree);
+	deleteFunc(&maintree, 10);
+	printf("\n");
+	printDerevo(&maintree);
+
+	
 	return 0;
 }
 
